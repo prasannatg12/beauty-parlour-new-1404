69eef32d5b9d27608bccbb607622b71ae16ae241
my pc <mypc@Prasannas-MacBook-Pro.local>
2026-04-14 09:01:39 +0530

    fix: vercel deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -55,7 +55,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: "dist", //path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
   server: {
